33b0b48def4feeb4db38c524689d98dab1c1d46e
leandro gomes <leandro-gssouza>
2020-05-25 19:59:46 -0300

    Movimento da cobrinha

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ snake[0] = {
     x: 1.25 * box,
     y: 1.25 * box
 }
+let direction = "right";
 
 function criarBG() {
     context.fillStyle ="lightgreen";
-    context.fillRect (20, 20, 25 * box, 25 * box);
+    context.fillRect (20, 20, 32 * box, 32 * box);
 }
 
 function criarCobrinha(){
@@ -19,5 +20,26 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY += box;
+    if (direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newhead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newhead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
