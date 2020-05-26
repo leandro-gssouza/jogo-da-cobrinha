ad202ecfe3a5b3cf2541499bcf61a01b3bdef7b6
leandro gomes <leandro-gssouza>
2020-05-26 18:59:28 -0300

    Jogo completo

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ function iniciarJogo(){
     if(snake[0].y > 31 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 32 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Você perdeu =(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
